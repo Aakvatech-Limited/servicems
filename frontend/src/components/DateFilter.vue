2adb122372387c6cf041670916a9b24384aa5c94
av-dev2 <emgani@aakvatech.com>
2024-11-05 09:47:58 +0300

    feat: Change Link appearance

--- frontend/src/components/DateFilter.vue
+++ frontend/src/components/DateFilter.vue
@@ -1,24 +1,21 @@
 <template>
     <div class="flex justify-around w-full  mt-4 h-15 border-2 rounded-2xl">
-        <!-- <h5 class="text-center mb-0 mt-0 font-bold">Date Filters</h5> -->
-        <div class="flex items-center mx-24">
-            <div class="flex-initial w-112">
+        <div class="flex items-center mx-24 gap-24">
+            <div class="flex-initial w-64 border-2 border-cyan-500/50 shadow-cyan-900/50 rounded-2xl">
                 <DatePicker
                     v-model="filters['from_date']"
                     variant="outline"
                     placeholder="Select From Date"
-                    label="From Date"
                 />
             </div>
-            <div class="flex-initial w-112">
+            <div class="flex-initial w-64 border-2 border-cyan-500/50 shadow-cyan-900/50 rounded-2xl">
                 <DatePicker
                     v-model="filters['to_date']"
                     variant="outline"
                     placeholder="Select To Date"
-                    label="To Date"
                 />
             </div>
-            <div class="flex-initial w-64">
+            <div class="flex-initial w-64 border-2 border-cyan-500/50 shadow-cyan-900/50 rounded-2xl">
                 <Link
                     class="form-control"
                     :value="filters['workshop']"
@@ -32,14 +29,9 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
 import Link from '../components/controls/Link.vue'
 
 defineProps({
     filters: Object,
 })
-
-// watch (() => props.fromDate, (newVal) => {
-//    = newVal
-// })
 </script>
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="h-screen w-60 bg-blue-400 flex flex-col items-start p-6">
     <Button 
-      class="mb-8 w-full bg-blue-500 hover:bg-blue-500/50 text-black font-bold py-6 px-4 rounded"
+      class="mb-8 w-full hover:bg-cyan-500 text-black font-bold py-6 px-4 rounded"
       label="Bookings"
       @click="goToBookings()"
     />
 
     <Button 
-      class="w-full bg-green-500 hover:bg-green-500 text-black font-bold py-6 px-4 rounded"
+      class="w-full hover:bg-green-500/50 text-black font-bold py-6 px-4 rounded"
       label="Job Card"
       @click="goToJobCard()"
     />
@@ -16,10 +16,10 @@
 
 <script setup>
 function goToBookings() {
-  window.location.href = '/app/Service Booking'
+  window.location.href = '/app/service-booking'
 }
 
 function goToJobCard() {
-  window.location.href = '/app/Service Job Card'
+  window.location.href = '/app/service-job-card'
 }
 </script>
